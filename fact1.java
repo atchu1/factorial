b0ffd36607abd0b76c4b7f3ee1ee11be1d35a4bf
atchu1 <atchayaraj18@gmail.com>
2019-02-27 13:49:34 +0530

    Create fact1.java

---
+++ fact1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Fact{
+public static void main(String args[]){
+ int i =1, factl=1, num;
+ System.out.println("Enter the number to which you need to find the factorial:");
+ Scanner sc = new Scanner(System.in);
+ num = sc.nextInt();
+ while(i <=number) {
+ fact= fact* i;
+i++;
+}
+System.out.println("Factorial of the given number is:: "+factorial);
+}
+}
