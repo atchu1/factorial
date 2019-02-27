fd92a81320f4af1ffe4f72d77d6671cd9ba4915c
atchu1 <atchayaraj18@gmail.com>
2019-02-27 14:20:05 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,10 @@
+class Odd {
+public static void main(String args[]) {
+System.out.println("The Odd Numbers are:");
+for (int i = 1;i<=6; i++){
+if (i % 2 != 0) {
+System.out.print(i + " ");
+}
+}
+}
+}
